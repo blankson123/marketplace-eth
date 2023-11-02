2d044629d7d94d74ec6840b8a96b8797ebf88216
Richmond Blankson <mensahrichmondb@gmail.com>
2023-11-02 13:51:10 +0000

    display course details with image

--- components/course/curriculum/index.js
+++ components/course/curriculum/index.js
@@ -1,12 +1,15 @@
-export default function Curriculum() {
-  const lectures = [
-    "How to init App",
-    "How to get a help",
-    "Introduction to Solidity",
-    "Programing in C++",
-    "How to write For Loops",
-    "Safe operator",
-  ];
+const lectures = [
+  "How to init App",
+  "How to get a help",
+  "Introduction to Solidity",
+  "Programing in C++",
+  "How to write For Loops",
+  "Safe operator",
+];
+
+export default function Curriculum({ locked }) {
+  const statusClass =
+    "px-2 inline-flex text-xs leading-5 font-semibold rounded-full";
 
   return (
     <section className="max-w-5xl mx-auto">
@@ -47,8 +50,14 @@ export default function Curriculum() {
                         </div>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap">
-                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                          Unlocked
+                        <span
+                          className={
+                            locked
+                              ? `bg-red-100 text-red-800 ${statusClass}`
+                              : `bg-green-100 text-green-800 ${statusClass}`
+                          }
+                        >
+                          {locked ? "Locked" : "Unlocked"}
                         </span>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
@@ -56,7 +65,7 @@ export default function Curriculum() {
                           href="#"
                           className="text-indigo-600 hover:text-indigo-900"
                         >
-                          Play
+                          {locked ? "Get Access" : "Play"}
                         </a>
                       </td>
                     </tr>

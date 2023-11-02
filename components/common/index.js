a4100dac88bacb13815dfc753eb39561f03997cc
Richmond Blankson <mensahrichmondb@gmail.com>
2023-11-02 10:36:12 +0000

    added advanced components for course

--- components/common/index.js
+++ components/common/index.js
@@ -2,3 +2,4 @@ export { default as Footer } from "./footer";
 export { default as Navbar } from "./navbar";
 export { default as Hero } from "./hero";
 export { default as BreadCrumb } from "./breadcrumb";
+export { default as Modal } from "./modal";
--- components/course/index.js
+++ components/course/index.js
@@ -1,1 +1,4 @@
 export { default as CourseList } from "./list";
+export { default as Curriculum } from "./curriculum";
+export { default as Hero } from "./hero";
+export { default as KeyPoints } from "./keypoints";

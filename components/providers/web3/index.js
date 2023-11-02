f644be549efc4e7306cf5feb4166853699d90e8b
Richmond Blankson <mensahrichmondb@gmail.com>
2023-11-02 16:11:44 +0000

    use account implemented

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -7,6 +7,7 @@ const {
 } = require("react");
 import detectEthereumProvider from "@metamask/detect-provider";
 import Web3 from "web3";
+import { setupHooks } from "./hooks/setupHooks";
 const Web3Context = createContext(null);
 
 export default function Web3Provider({ children }) {
@@ -36,6 +37,7 @@ export default function Web3Provider({ children }) {
     return {
       ...web3Api,
       isWeb3Loaded: web3Api.web3 != null,
+      hooks: setupHooks(web3Api.web3),
       connect: web3Api.provider
         ? async () => {
             try {

2738c86b6ff633334213fe31f8f76e6cb774351d
Richmond Blankson <mensahrichmondb@gmail.com>
2023-11-03 01:22:40 +0000

    add address on marketplace page

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -2,11 +2,13 @@ import Link from "next/link";
 import { useWeb3 } from "@components/providers";
 import { Button } from "@components/ui/common";
 import { useAccount } from "@components/hooks/web3/useAccount";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
   const { connect, isLoading, isWeb3Loaded } = useWeb3();
 
   const { account } = useAccount();
+  const { pathname } = useRouter();
 
   return (
     <section>
@@ -64,7 +66,7 @@ export default function Navbar() {
         </nav>
       </div>
 
-      {account.data && (
+      {account.data && !pathname.includes("/marketplace") && (
         <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
           <div className="text-white bg-indigo-600 rounded-md p-2">
             {account.data}

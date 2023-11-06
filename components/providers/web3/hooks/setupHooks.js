d49d79ba3cb10d04b53a16715a96e168148be2fd
Richmond Blankson <mensahrichmondb@gmail.com>
2023-11-06 09:24:07 +0000

    usenetwork skeleton created

--- components/providers/web3/hooks/setupHooks.js
+++ components/providers/web3/hooks/setupHooks.js
@@ -1,7 +1,9 @@
 import { handler as createUseAccount } from "./handler";
+import { handler as createUseNetwork } from "./useNetwork";
 
 export const setupHooks = (web3, provider) => {
   return {
     createUseAccount: createUseAccount(web3, provider),
+    createUseNetwork: createUseNetwork(web3, provider),
   };
 };

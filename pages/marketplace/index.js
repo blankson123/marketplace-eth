da169a33b06101f96abc10f0a5490cf7b21c7ee6
Richmond Blankson <mensahrichmondb@gmail.com>
2023-11-09 12:27:35 +0000

    extract course card to its component

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -1,6 +1,6 @@
 import { useAccount } from "@components/hooks/web3/useAccount";
 import { useNetwork } from "@components/hooks/web3/useNetwork";
-import { CourseList } from "@components/ui/course";
+import { CourseCard, CourseList } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { WalletBar } from "@components/ui/web3";
 import { getAllCourses } from "@content/courses/fetcher";
@@ -13,7 +13,9 @@ export default function Marketplace({ courses }) {
       <div className="py-4">
         <WalletBar address={account.data} network={network?.data} />
       </div>
-      <CourseList courses={courses} />
+      <CourseList courses={courses}>
+        {(course) => <CourseCard key={course.id} course={course} />}
+      </CourseList>
     </>
   );
 }

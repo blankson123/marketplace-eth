c60785eaa4b69bb76928539553988a38f3e247f5
Richmond Blankson <mensahrichmondb@gmail.com>
2023-11-13 21:36:39 +0000

    implement the market pages

--- components/ui/common/breadcrumb/index.js
+++ components/ui/common/breadcrumb/index.js
@@ -1,16 +1,24 @@
-export default function BreadCrumb() {
+import Link from "next/link";
+
+export default function BreadCrumb({ items }) {
+  const rederredItems = items.map((item, i) => {
+    return (
+      <>
+        <li
+          key={item.href}
+          className={`${
+            i == 0 ? "pr-4" : "px-4"
+          } font-medium text-gray-500 hover:text-gray-900`}
+        >
+          <Link href={item.href}>{item.value}</Link>
+        </li>
+      </>
+    );
+  });
   return (
     <nav aria-label="breadcrumb">
       <ol className="flex leading-none text-indigo-600 divide-x divide-indigo-400">
-        <li className="pr-4 font-medium  text-gray-500 hover:text-gray-900">
-          <a href="#">Buy</a>
-        </li>
-        <li className="px-4 font-medium  text-gray-500 hover:text-gray-900">
-          <a href="#">My Courses</a>
-        </li>
-        <li className="px-4 font-medium  text-gray-500 hover:text-gray-900">
-          <a href="#">Manage Courses</a>
-        </li>
+        {rederredItems}
       </ol>
     </nav>
   );
--- components/ui/common/marketplace/header/index.js
+++ components/ui/common/marketplace/header/index.js
@@ -1,13 +1,19 @@
 import { EthRates, WalletBar } from "@components/ui/web3";
 import { BreadCrumb } from "@components/ui/common";
 
+const LINKS = [
+  { href: "/marketplace", value: "Buy" },
+  { href: "/marketplace/courses/owned", value: "My Courses" },
+  { href: "/marketplace/courses/manage", value: "Manage Courses" },
+];
+
 export default function Header() {
   return (
     <>
       <WalletBar />
       <EthRates />
       <div className="flex flex-row-reverse pb-4 px-4 sm:px-6 lg:px-8">
-        <BreadCrumb />
+        <BreadCrumb items={LINKS} />
       </div>
     </>
   );

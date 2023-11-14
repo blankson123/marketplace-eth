1a5fa559771e46b196c050c668f389157047292f
Richmond Blankson <mensahrichmondb@gmail.com>
2023-11-14 10:05:24 +0000

    store courses in contract

--- contracts/CourseMarketplace.sol
+++ contracts/CourseMarketplace.sol
@@ -14,4 +14,27 @@ contract CourseMarketplace{
         address owner; // 20 bytes
         State state; // 1 byte
     }
+
+    // mapping of courseHash to course data
+    mapping(bytes32 => Course) private ownedCourses;
+
+    // mapping of courseID to courseHash
+    mapping(uint => bytes32) private ownedCourseHash;
+
+    // number of all courses + id of the coruse
+    uint private totalOwnedCourses;
+
+    // external means - to be only called outside of the smart contract
+    function purchaseCourse(bytes16 courseId, bytes32 proof) external payable{
+        bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
+        uint id = totalOwnedCourses++;
+        ownedCourseHash[id] = courseHash;
+        ownedCourses[courseHash] = Course({
+            id: id,
+            price: msg.value,
+            proof: proof,
+            owner: msg.sender,
+            state: State.Purchased
+        });
+    }
 }
